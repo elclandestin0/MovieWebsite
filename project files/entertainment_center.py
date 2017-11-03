181a5f1a6a14d42cd81b99c4244d74891801db16
Memo Khoury <memokhoury@Memos-Air.local>
2017-11-03 12:34:52 -0400

    Added comments, with regards to the Google Python style guide

--- project files/entertainment_center.py
+++ project files/entertainment_center.py
@@ -1,8 +1,12 @@
 import media
 import fresh_tomatoes
-import webbrowser
-import urllib2
 import tmdbsimple as tmdb
+
+""" This is the driver file. It creates the movie instances,
+    populates the data using The Movie Database as an API,
+    and then feeds the array of instances into a function
+    in fresh_tomatoes, ultimately compiling it into a website.
+"""
 
 tmdb.API_KEY = '4a3e0dba52dfa69ee42550261e86155f'
 
@@ -70,6 +74,26 @@ movies_search_string = ['Toy Story',
                         'Jurassic Park']
 
 def populate_data(moviesList):
+    """Fetches data of movie from The Movie Database using TMDB API.
+
+        Retrieves the unique tuple that we need based on the array of string
+        movies above. After selecting the movie based on the search, it takes
+        it's title, overview, and poster image. This data is fed back to the
+        movie instance.
+
+    Args:
+        moviesList: An array of movie instances we created before. Each
+        instance's parameters are all empty, except the Youtube URL.
+        This is because we want to fetch/overwrite the other data using
+        this function.
+    """
+    # For each item in the array of strings containing the movie title,
+    # we request a search from TMDB API using that item in the array.
+    # For the first item that comes in the search, we fetch the movie's
+    # title, storyline, and poster image and set it to the corresponding
+    # movie instance, which is in an array of movie instances created
+    # above. The loop breaks after the first search because it is the
+    # most confident item in the query.
     for m in range(len(moviesList)):
         search = tmdb.Search()
         response = search.movie(query = movies_search_string[m])
--- project files/media.py
+++ project files/media.py
@@ -1,19 +1,23 @@
 ## Code written  by: Memo Khoury
-# Start date: <insert date>
-# Last modified: <insert date>
-#
-# Comments are numbered to the right of the lines of code,
-# to understand each comment, kindly scroll to the
-# bottom of the file.
+# Start date: 11/01/2017
+# Last modified: 11/03/2017
 
 import webbrowser
 
 class Movie():
-    """ This class defines a movie with four attributes: it's title, trailer, poster, and storyline. It also has a function to show the trailer """
+    """ This class defines a movie with four attributes:
+        it's title, trailer, poster, and storyline. It also
+        has a function to show the trailer
+    Attributes:
+              movie_title: A string that contains the movie title.
+              trailer_youtube_url: A string containing the URL link of the trailer.
+              storyline: A string that contains a movie's description/overview.
+              poster_image_url: A string of a directory path that contains the image.
+    """
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #1
+    #VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, trailer_youtube_url, storyline, poster_image_url):  #2
+    def __init__(self, movie_title, trailer_youtube_url, storyline, poster_image_url):
         self.title = movie_title
         self.trailer_youtube_url = trailer_youtube_url
         self.storyline = storyline
@@ -21,10 +25,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-
-
-
-##### List of Comments #####
-#1: CONSTANT VARIABLES ARE ALL CAPS BY NAME DECLARATION
