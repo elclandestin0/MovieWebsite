d5c49e0aba344adb3314bf3339efa9de2a8737df
Memo Khoury <memokhoury@Memos-Air.local>
2017-11-04 23:48:50 -0400

    Changed formatting according to the PEP8 style guidelines

--- project files/entertainment_center.py
+++ project files/entertainment_center.py
@@ -1,5 +1,6 @@
 # Code written by: Memo Khoury under the guidance of the Udacity team
-# Modules used: media, fresh_tomatoes, and The Movie Database API (https://www.themoviedb.org)
+# Modules used: media, fresh_tomatoes, and
+# The Movie Database API (https://www.themoviedb.org)
 # Start date: 11/01/2017
 # Last modified: 11/03/2017
 
@@ -15,48 +16,60 @@ import tmdbsimple as tmdb
 
 tmdb.API_KEY = '4a3e0dba52dfa69ee42550261e86155f'
 
-toy_story = media.Movie("",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc",
-                        "",
-                        "")
+toy_story =
+media.Movie("",
+            "https://www.youtube.com/watch?v=KYz2wyBy3kc",
+            "",
+            "")
+avatar =
+media.Movie("",
+            "https://www.youtube.com/watch?v=d1_JBMrrYw8",
+            "",
+            "")
 
-avatar = media.Movie("",
-                     "https://www.youtube.com/watch?v=d1_JBMrrYw8",
-                     "",
-                     "")
+la_haine =
+media.Movie("",
+            "https://www.youtube.com/watch?v=cy5TaO6qTBU",
+            "",
+            "")
 
-la_haine = media.Movie("",
-                       "https://www.youtube.com/watch?v=cy5TaO6qTBU",
-                       "",
-                       "")
+reservoir_dogs =
+media.Movie("",
+            "https://www.youtube.com/watch?v=vayksn4Y93A",
+            "",
+            "")
 
-reservoir_dogs = media.Movie("",
-                             "https://www.youtube.com/watch?v=vayksn4Y93A",
-                             "",
-                             "")
-
-the_dark_knight = media.Movie("",
-                              "https://www.youtube.com/watch?v=EXeTwQWrcwY",
-                              "",
-                              "")
+the_dark_knight =
+media.Movie("",
+            "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+            "",
+            "")
 
 
-spirited_away = media.Movie("",
-                            "https://www.youtube.com/watch?v=ByXuk9QqQkk",
-                            "",
-                            "")
-fight_club = media.Movie("",
-                         "https://www.youtube.com/watch?v=SUXWAEX2jlg",
-                         "",
-                         "")
-the_departed = media.Movie("",
-                           "https://www.youtube.com/watch?v=iojhqm0JTW4",
-                           "",
-                           "")
-jurassic_park = media.Movie("",
-                           "https://www.youtube.com/watch?v=lc0UehYemQA&t=66s",
-                           "",
-                           "")
+spirited_away =
+media.Movie("",
+            "https://www.youtube.com/watch?v=ByXuk9QqQkk",
+            "",
+            "")
+
+fight_club =
+media.Movie("",
+            "https://www.youtube.com/watch?v=SUXWAEX2jlg",
+            "",
+            "")
+
+the_departed =
+media.Movie("",
+            "https://www.youtube.com/watch?v=iojhqm0JTW4",
+            "",
+            "")
+
+jurassic_park =
+media.Movie("",
+            "https://www.youtube.com/watch?v=lc0UehYemQA&t=66s",
+            "",
+            "")
+
 movies = [toy_story,
           avatar,
           la_haine,
@@ -104,14 +117,13 @@ def populate_data(movies_list):
 
     for m in range(len(movies_list)):
         search = tmdb.Search()
-        response = search.movie(query = movies_search_string[m])
+        response = search.movie(query=movies_search_string[m])
         for s in search.results:
             movies_list[m].title = s['title']
             movies_list[m].storyline = s['overview']
-            movies_list[m].poster_image_url = "https://image.tmdb.org/t/p/w500" + s['poster_path']
+            movies_list[m].poster_image_url =
+            "https://image.tmdb.org/t/p/w500" + s['poster_path']
             break
-
-
 
 populate_data(movies)
 fresh_tomatoes.open_movies_page(movies)
--- project files/media.py
+++ project files/media.py
@@ -5,24 +5,26 @@
 
 import webbrowser
 
+
 class Movie():
     """ This class defines a movie with four attributes:
         it's title, trailer, poster, and storyline. It also
         has a function to show the trailer
     Attributes:
               movie_title: A string that contains the movie title.
-              trailer_youtube_url: A string containing the URL link of the trailer.
-              storyline: A string that contains a movie's description/overview.
-              poster_image_url: A string of a directory path that contains the image.
+              trailer_youtube_url: A string containing the URL link of
+              the trailer.
+              storyline: A string that contains a movie's
+              description/overview.
+              poster_image_url: A string of a directory path
+              that contains the image.
     """
 
-    #VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-    def __init__(self, movie_title, trailer_youtube_url, storyline, poster_image_url):
+    def __init__(self, movie_title, youtube_url, storyline, poster_url):
         self.title = movie_title
-        self.trailer_youtube_url = trailer_youtube_url
+        self.youtube_url = youtube_url
         self.storyline = storyline
-        self.poster_image_url = poster_image_url
+        self.poster_url = poster_url
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
